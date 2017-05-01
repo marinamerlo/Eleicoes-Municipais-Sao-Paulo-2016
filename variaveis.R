aba625d77400f3c36816f9f78a5c99f42da03a84
Marina Merlo <marina27merlo@gmail.com>
2017-05-01 19:43:56 -0300

    Update variaveis.R

--- variaveis.R
+++ variaveis.R
@@ -145,12 +145,114 @@ dados <- dados %>%
   mutate(cand_situ_fem_pct = (cand_situ_fem / cand_situ))
 
 
+
+################################
+#####VARIÁVEIS DE RECURSOS#####
+###############################
+
+#renomeando as variáveis de recursos para nomes mais curtos
+make.names(names(dados))
+
+dados <- dados %>%
+  rename(reccom = `valor.Comercialização de bens ou realização de eventos`,
+         recint = `valor.Doações pela Internet`,
+         recni = `valor.Recursos de origens não identificadas`,
+         reccand = `valor.Recursos de outros candidatos`,
+         recpart = `valor.Recursos de partido político`,
+         recfis = `valor.Recursos de pessoas físicas`,
+         recprop = `valor.Recursos próprios`,
+         recfin = `valor.Rendimentos de aplicações financeiras`)
+
+#variável que tem o total de recursos recebidos
+dados <- dados %>% 
+  rowwise() %>% 
+  mutate(rectotal = sum(as.numeric(reccom), 
+                        as.numeric(recint), 
+                        as.numeric(recni), 
+                        as.numeric(reccand), 
+                        as.numeric(recpart),
+                        as.numeric(recfis),
+                        as.numeric(recprop),
+                        as.numeric(recfin), na.rm=TRUE))
+
+#vendo se ficou ok
+summary(dados$rectotal)
+
+
+#tirando o log dos recursos totais
+dados$rectotal.log <- as.numeric(log(dados$rectotal))
+#transformando o -Inf em zero, pois é o resultado do log de 0
+dados$rectotal.log[is.infinite(dados$rectotal.log) ] <- 0
+dados$rectotal.log[dados$rectotal.log < 0] <- 0.0000000000000000000001
+summary(dados$rectotal.log)
+
+
+#tirando o log dos recursos partidários
+dados$recpart.log <- as.numeric(log(dados$recpart))
+#transformando o -Inf em zero, pois é o resultado do log de 0
+dados$recpart.log[is.infinite(dados$recpart.log) ] <- 0
+dados$recpart.log[dados$recpart.log < 0] <- 0.0000000000000000000001
+summary(dados$recpart.log)
+
+#tirando o log dos recursos de outros candidatos
+dados$reccand.log <- as.numeric(log(dados$reccand))
+#transformando o -Inf em zero, pois é o resultado do log de 0
+dados$reccand.log[is.infinite(dados$reccand.log) ] <- 0
+dados$reccand.log[dados$reccand.log < 0] <- 0.0000000000000000000001
+summary(dados$reccand.log)
+
+
+#variável pra indicar se recebeu recursos ou não
+dados$recurso.f <- ifelse(dados$rectotal == 0, c("Nao Recebeu Recursos"), c("Recebeu Recursos"))
+dados$recurso.f <- as.factor(dados$recurso.f)
+summary(dados$recurso.f)
+
 #variável para identificar candidaturas sem votos e sem recursos
 dados$laranja.f <- ifelse(dados$recurso.f == "Nao Recebeu Recursos" & (dados$votos_total_cand == 0), c("Laranja"), c("0"))
 table(dados$laranja.f)
 dados$laranja.f <- as.factor(dados$laranja.f)
 summary(dados$laranja.f)
 
+
+#variável pra indicar se recebeu recursos do partido ou não
+dados$recurso.part.f <- ifelse(dados$recpart > 0, c("1"), c("0"))
+dados$recurso.part.f <- as.factor(dados$recurso.part.f)
+summary(dados$recurso.part.f)
+
+#variável pra indicar se recebeu votos ou não
+dados$voto.f <- ifelse(dados$votos_total_cand == 0, c("0"), c("1"))
+dados$voto.f <- as.factor(dados$voto.f)
+#Deixando os NA como zero porque representam não ter recebido votos em 2014:
+dados$voto.f[is.na(dados$voto.f)] <- 0
+summary(dados$voto.f)
+
+
+
+
+#soma de recursos partidários por partido
+recursos_partido <- dados %>%
+  filter(recpart > 0 | recpart != NA) %>%
+  group_by(sigla) %>%
+  summarise(recpart_total = sum(recpart))
+
+#soma de recursos partidários por partido para mulheres
+recursos_partido_fem <- dados %>%
+  filter(genero == "Feminino") %>%
+  filter(recpart > 0 | recpart != NA) %>%
+  group_by(sigla) %>%
+  summarise(recpart_total_fem = sum(recpart))
+
+
+##juntando no banco de dados
+
+dados <- dados %>%
+  left_join(recursos_partido, by = "sigla") %>%
+  left_join(recursos_partido_fem, by = "sigla")
+
+
+#fazendo as variáveis de proporção de candidaturas femininas por partido e coligação
+dados <- dados %>%
+  mutate(recpart_fem_pct = (recpart_total_fem / recpart_total))
 
 
 
