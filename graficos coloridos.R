0930efab1b1d66dd6c8e20a6330baa40c52522d9
Marina Merlo <marina27merlo@gmail.com>
2017-06-19 00:03:33 -0300

    Update graficos coloridos.R

--- graficos coloridos.R
+++ graficos coloridos.R
@@ -3,18 +3,56 @@ library(ggrepel)
 setwd("D:/Dropbox/Mestrado/Seminário Discente/2017/graficos")
 
 
+##densidade e boxplot por gênero e resultado
+kdensity <- ggplot(data=dados, aes(x=voto_total_cand.log, fill=factor(genero))) +
+  geom_density(alpha=0.4, size = 0.05, colour = "gray30") +
+  theme_minimal()+
+  scale_fill_manual(name = "Gênero", values = c("maroon4", "seagreen")) +
+  labs(title = "Densidade dos Votos",
+       x = "Votos Totais (log)",
+       y = NULL) + 
+  facet_grid(. ~eleito) + 
+  theme(legend.position = "none",
+        plot.title = element_text(hjust = 0.5)) + 
+  scale_y_reverse() + 
+  coord_flip()
+kdensity
+
+boxplot <- ggplot(dados, aes(genero, voto_total_cand.log, fill=factor(genero))) +
+  theme_minimal()+
+  geom_boxplot(alpha=0.4, lwd=0.1, colour = "gray30")+
+  scale_fill_manual(name = "Gênero", values = c("maroon4", "seagreen")) +
+  facet_grid(. ~eleito) +
+labs(title = "Distribuição dos votos",
+                            y = "Votos Totais (log)",
+                            x = "Gênero") + 
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        plot.title = element_text(hjust = 0.5)) +
+  theme(legend.position = "bottom")
+
+boxplot
+
+library(gridExtra)
+grid.arrange(kdensity, boxplot, ncol=1)
+ggsave("boxplot_kdensity_votos_resultado_genero.png", width = 5, height = 5, arrangeGrob(bplot2, bplot3, ncol=1))
+
+
 ##gráfico de barras de candidaturas por coligação e por gênero
 g <-ggplot(data = dados, aes(x = reorder(colig, cand_colig_fem_pct),  y = cand_colig, fill = genero)) + 
   geom_bar(stat = "identity", position = "fill") +
-  theme_bw()+
+  theme_minimal()+
   scale_fill_manual(name = "Gênero", values = c("maroon4", "seagreen")) +
   geom_hline(yintercept = 0.7) +
   labs(title ="Gênero das candidaturas por Coligação", x = "Coligação", y = "% de candidaturas") + 
-  theme(axis.text.x=element_text(angle=50, hjust=1)) +
-  coord_flip()
-
-g
-ggsave("barplot_candidaturas_genero_colig_cor.png", width = 10, height = 5)
+  theme(axis.text.x=element_text(angle=50, hjust=1), 
+        legend.position = "bottom") +
+  coord_flip() +
+  scale_y_reverse(breaks = c(0, 0.25, 0.5, 0.75, 1),
+                labels = c("100%","75%","50%", "25%", "0%"))
+g 
+ggsave("barplot_candidaturas_genero_colig_cor.png", width = 8, height = 5)
 
 ##gráfico de barras de candidaturas por partido e por gênero 
 g <-ggplot(data = dados, aes(x = reorder(sigla, cand_part_fem_pct),  y = cand_part, fill = genero)) + 
@@ -23,11 +61,13 @@ g <-ggplot(data = dados, aes(x = reorder(sigla, cand_part_fem_pct),  y = cand_pa
   scale_fill_manual(name = "Gênero", values = c("maroon4", "seagreen")) +
   geom_hline(yintercept = 0.7) +
   labs(title ="Gênero das candidaturas por Partido", x = "Partido", y = "% de candidaturas") + 
-  theme(axis.text.x=element_text(angle=50, hjust=1)) +
-  coord_flip()
-
+  theme(axis.text.x=element_text(angle=50, hjust=1), 
+        legend.position = "bottom") +
+  coord_flip() +
+  scale_y_reverse(breaks = c(0, 0.25, 0.5, 0.75, 1),
+                labels = c("100%","75%","50%", "25%", "0%"))
 g
-ggsave("barplot_candidaturas_genero_partido_cor.png", width = 10, height = 5)
+ggsave("barplot_candidaturas_genero_partido_cor.png", width = 8, height = 5)
 
 library(ggrepel)
 p <- ggplot(dados_semsuplicy, aes(x = eleito, y = votos_total_cand, alpha=as.factor(genero))) +
