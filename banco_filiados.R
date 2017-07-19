5bdccf1e0883032b62e232460dd778be3e3e5dd8
Marina Merlo <marina27merlo@gmail.com>
2017-07-19 18:41:20 -0300

    Update banco_filiados.R

--- banco_filiados.R
+++ banco_filiados.R
@@ -262,3 +262,17 @@ filiados_reg <- filiados_reg %>%
   left_join(filia_indet, by = "sigla") %>%
   left_join(filia_total, by = "sigla") %>%
   mutate(pct = n_mulheres_partido / n_partido)
+
+
+#resumo dos partidos
+filia_partidos <- filia_total %>%
+  left_join(filia_mulheres, by = "sigla") %>%
+  left_join(filia_homens, by = "sigla") %>%
+  left_join(filia_indet, by = "sigla") %>%
+  mutate(fem_masc = (n_mulheres_partido / n_homens_partido)*100) %>%
+  mutate(pct_fem = (n_mulheres_partido / n_partido)*100) %>%
+  mutate(pct_masc = (n_homens_partido / n_partido)*100) %>%
+  mutate(pct_indet = (n_indet_partido / n_partido)*100)
+
+#salvando a tabela final dos resumos dos partidos
+write.table(filia_partidos, "filiados_partidos.csv", sep = ";", dec=",", fileEncoding ="UTF-8", row.names = F)
