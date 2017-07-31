1c16b8b83cce2165469b43dd8b25e8248a011205
Marina Merlo <marina27merlo@gmail.com>
2017-07-30 21:42:20 -0300

    Update banco_filiados.R

--- banco_filiados.R
+++ banco_filiados.R
@@ -1,5 +1,10 @@
+#O objetivo desse script é baixar e organizar os dados de filiados da cidade de São Paulo, disponibilizados pelo TSE. Também
+#é feita a classificação de gênero dos filiados a partir dos seus nomes.
+
+#indicando a pasta onde ficará salvo
 setwd("C:\\Users\\d841255\\Desktop\\filiados")
 
+#carregando os pacotes necessários
 library(readr)
 library(dplyr)
 library(data.table)
@@ -9,21 +14,78 @@ library(stringi)
 library(gender)
 library(tidyverse)
 
-lista.arquivos <-list.files(file.path(getwd()))
+#Quando o site "http://www.tse.jus.br/partidos/filiacao-partidaria/relacao-de-filiados" é acessado, é possível ver que a estrutura
+#do link para baixar o .zip da relação de filiados é:
+#http://agencia.tse.jus.br/estatistica/sead/eleitorado/filiados/uf/filiados_dem_sp.zip
+#ou seja, só muda o partido e o estado. Criando um loop para gerar os links  de cada partido, dá pra baixar todos de uma vez sem
+#precisar clicar 34 vezes (!) para baixar a relação de todos os partidos.
+
+#criando a relação de partidos para gerar os links. Também seria possível criar uma relação por estados para o loop, 
+#mas como estou interessada somente na cidade de São Paulo, não vou fazer para baixar todos. 
+partidos <- c("novo",
+              "pen",
+              "pc_do_b",
+              "pcb",
+              "pco",
+              "pdt",
+              "phs",
+              "pmdb",
+              "pmb",
+              "pmn",
+              "pp",
+              "ppl",
+              "pps",
+              "pr",
+              "prb",
+              "pros",
+              "prp",
+              "prtb",
+              "psb",
+              "psc",
+              "psd",
+              "psdb",
+              "psdc",
+              "psl",
+              "psol",
+              "pstu",
+              "pt",
+              "pt_do_b",
+              "ptb",
+              "ptc",
+              "ptn",
+              "pv",
+              "rede",
+              "sd")
+
+#criando o link base
+url_base <- "http://agencia.tse.jus.br/estatistica/sead/eleitorado/filiados/uf/filiados_PARTIDO_sp.zip"
+
+#loop para criar os links, baixar o .zip em um arquivo temporário e extrair seu conteúdo:
+  for (partido in partidos){
+  print(partido)
+  url <- gsub("PARTIDO", partido, url_base)
+  download.file(url, "temp.zip", quiet = F)
+  unzip("temp.zip")
+  file.remove("temp.zip")
+} 
+
+#vendo como ficou a pasta depois de descompactar:
+list.dirs()
+#pegando a última pasta da árvore, que é onde estão os arquivos .csv
+lista.arquivos<- list.files("./aplic/sead/lista_filiados/uf")
+print(lista.arquivos)
+#os arquivos que estão indicados com _jud_ se referem às pendências judiciais da relação de filiados. 
+#Como o interesse é apenas nos filiados regulares, não é preciso trabalhar com esses nomes.
+#criando uma lista de arquivos apenas com a relação de filiados normal
 lista.banco <- grep(pattern="filiados_", lista.arquivos, value=TRUE)
 
-filiados <- data.frame()
-
-for (arquivo in lista.banco) {
-  unzip(arquivo)
-  files <- list.files(path = "C:\\Users\\d841255\\Desktop\\filiados\\aplic\\sead\\lista_filiados\\uf\\", pattern="filiados_", full.names = TRUE)
-}
-
-for (file in files) {
+#juntando todos os arquivos em um só, já filtrando pelo município de SP para acelerar a junção.
+#(ainda assim, demora um pouco. pegue um café, dê uma volta!)
+for (file in lista.banco) {
   dado <- fread(file) 
   dado <- dado %>%
     filter(`NOME DO MUNICIPIO` == "SÃO PAULO")
-    filiados<-bind_rows(filiados, dado)
+  filiados<-bind_rows(filiados, dado)
 }
 
 #por alguma razão ele ta criando com linhas repetidas! dando o distinct, o dado fica certo.
@@ -51,18 +113,28 @@ filiados <- filiados %>%
          data_cancel = `DATA DO CANCELAMENTO`,
          data_regula = `DATA DA REGULARIZACAO`,
          motivo_cancel = `MOTIVO DO CANCELAMENTO`) %>%
-  #extrai apenas o primeiro nome do nome completo, retira acentos e deixa em minúsculo       
+  #extrai apenas o primeiro nome do nome completo, retira acentos e deixa em minúsculo.
+  #essa transformação será utilizada no próximo passo para a classificação de gênero.
   mutate(primeironome = word(nome, 1)) %>%
   mutate(primeironome = tolower(stri_trans_general(primeironome, "Latin-ASCII")))
 
-#salvando o banco gerado
-write.table(filiados, "filiados_SP.csv", sep = ";", fileEncoding ="latin1", row.names = F)
+#salvando o banco gerado até agora
+#write.table(filiados, "filiados_SP.csv", sep = ";", fileEncoding ="latin1", row.names = F)
+
+#################################################################
+#### CLASSIFICAÇÃO DE GÊNERO DOS FILIADOS PELO PRIMEIRO NOME ####
+#################################################################
+
+#Para conseguir classificar o gênero dos filiados, serão feitas duas etapas:
+#primeiro, a partir dos dados de candidatura do Brasil todo de 2016
+#em segundo, com os nomes que não forem classificados com o primeiro método,
+#usando o pacote gender, que classifica os nomes de acordo com os dados do Censo Americano de 1932 a 2012.
+#o objetivo desse segundo passo é dar conta de classificar nomes mais incomuns ou estrangeiros que não constem
+#na relação de candidatos em 2016.
 
 
-###pegando os nomes dos candidatos do brasil todo (socorro!)
-
+###pegando os nomes dos candidatos do brasil todo (demora um pouco)
 setwd("C:\\Users\\d841255\\Desktop\\candidatos")
-
 #baixando o arquivo com os dados de candidaturas
 url_cand <- "http://agencia.tse.jus.br/estatistica/sead/odsele/consulta_cand/consulta_cand_2016.zip"
 download.file(url_cand, "temp.zip", quiet = F)
@@ -70,13 +142,14 @@ download.file(url_cand, "temp.zip", quiet = F)
 unzip("temp.zip")
 file.remove("temp.zip")
 
-##repetindo os passos para criar o arquivo das candidaturas##
 #criando uma lista de todos os arquivos contidos na pasta
 lista.arquivos <-list.files(file.path(getwd()))
 print(lista.arquivos)
+
 #criando uma lista para pegar somente os documentos de votação
 lista.candidatos <- grep(pattern="consulta_cand_2016_", lista.arquivos, value=TRUE)
 print(lista.candidatos)
+
 #criando o dataframe vazio que receberá os dados
 candidatos <- data.frame()
 
@@ -138,36 +211,41 @@ colnames(candidatos)<- c("DATA_GERACAO",
                          "DESC_SIT_TOT_TURNO")
 
 
-
+#criando um banco que contenha somente o nome e o sexo do candidato
 nomes <- candidatos %>%
   select(NOME_CANDIDATO, DESCRICAO_SEXO) %>%
   rename(sexo = DESCRICAO_SEXO,
          nome_comp = NOME_CANDIDATO)
 
 
-#deixando uma lista única de primeiros nomes por gênero
+#deixando uma lista única de primeiros nomes por gênero, a ser utilizada para o match
 nomes <- nomes %>%
   mutate(primeironome = word(nome_comp, 1)) %>%
   mutate(primeironome = tolower(stri_trans_general(primeironome, "Latin-ASCII"))) %>%
   select(-nome_comp)%>%
   distinct()
 
+#alguns nomes são marcados tanto como FEMININO quanto MASCULINO. Para obter quais são esses nomes:
 nomes$duplicado <- duplicated(nomes$primeironome)
 unico <- unique(nomes$primeironome)
 
+ 
+#checando quais são os nomes duplicados
 duplicados <- nomes %>%
-  filter(duplicado == "TRUE") %>%
-  select(-sexo)
+  filter(duplicado == "TRUE")
+#Em checagem aleatória, todos os nomes duplicados marcados como
+#TRUE eram classificações de sexo mais incomuns. Por exemplo, o nome "adriana" foi classificado como "FEMININO"; a classificação
+#do mesmo nome como "MASCULINO" fica marcada como TRUE quando damos o duplicado.
 
+#removendo os nomes de classificação duplicada. 
 nomes <- nomes %>%
   filter(duplicado == FALSE)
-
 
 #juntando no banco de filiados
 filiados <- filiados %>%
   left_join(nomes, by = "primeironome")
 
-#vendo o que falta
+#vendo quais nomes faltaram ser classificados
 falta <- filiados %>%
   filter(is.na(sexo))
 
@@ -175,10 +253,10 @@ falta <- filiados %>%
 falta_gen <- falta %>%
   select(insc,primeironome)
 
-#aplicando o algoritmo do pacote gender
+#aplicando o algoritmo do pacote gender nesses nomes
 gender <- gender(falta_gen$primeironome, method = "ssa")
 
-#vendo se as proporções estão similares
+#vendo se as proporções entre homens e mulheres estão similares entre os dois métodos
 table(filiados$sexo)
 table(gender$gender)
 
@@ -206,73 +284,15 @@ filia_completo <- bind_rows(filia_gender, filia_cand) %>%
   distinct()
 
 
-#padronizando a codificação da variável
+#padronizando a codificação da variável, para indicar como "INDETERMINADO" aqueles que não foram classificados
+#por nenhum dos métodos
 filia_completo <- filia_completo %>%
   mutate(sexo = ifelse(sexo == "female", "FEMININO", sexo)) %>%
   mutate(sexo = ifelse(sexo == "male", "MASCULINO", sexo)) %>%
   mutate(sexo = ifelse(is.na(sexo), "INDETERMINADO", sexo))
 
-#vendo como ficou
+#vendo como ficou - 3,22% dos nomes não puderam ser classificados em nenhum dos métodos.
 table(filia_completo$sexo)
 
 #salvando a tabela final
 write.table(filia_completo, "filiados_SP_completo.csv", sep = ";", fileEncoding ="UTF-8", row.names = F)
-
-#limpando a área
-rm(falta_gen)
-rm(filia_cand)
-rm(filia_gender)
-rm(gender)
-rm(genero_gender)
-rm(filiados)
-rm(falta)
-rm(candidatos)
-rm(dado)
-rm(nomes)
-
-##fazendo as variáveis!
-
-###refazer as contagens considerando apenas os filiados regulares
-filiados_reg <- filia_completo %>%
-  filter(situ_reg == "REGULAR")
-
-filia_mulheres <- filiados_reg %>%
-  filter(sexo == "FEMININO") %>%
-  group_by(sigla) %>%
-  summarise(n_mulheres_partido = n())
-
-filia_homens <- filiados_reg %>%
-  filter(sexo == "MASCULINO") %>%
-  group_by(sigla) %>%
-  summarise(n_homens_partido = n())
-
-filia_indet <- filiados_reg %>%
-  filter(sexo == "INDETERMINADO") %>%
-  group_by(sigla) %>%
-  summarise(n_indet_partido = n())
-
-filia_total <- filiados_reg %>%
-  group_by(sigla) %>%
-  summarise(n_partido = n())
-
-filiados_reg <- filiados_reg %>%
-  mutate(n_total = n()) %>%
-  left_join(filia_mulheres, by = "sigla") %>%
-  left_join(filia_homens, by = "sigla") %>%
-  left_join(filia_indet, by = "sigla") %>%
-  left_join(filia_total, by = "sigla") %>%
-  mutate(pct = n_mulheres_partido / n_partido)
-
-
-#resumo dos partidos
-filia_partidos <- filia_total %>%
-  left_join(filia_mulheres, by = "sigla") %>%
-  left_join(filia_homens, by = "sigla") %>%
-  left_join(filia_indet, by = "sigla") %>%
-  mutate(fem_masc = (n_mulheres_partido / n_homens_partido)*100) %>%
-  mutate(pct_fem = (n_mulheres_partido / n_partido)*100) %>%
-  mutate(pct_masc = (n_homens_partido / n_partido)*100) %>%
-  mutate(pct_indet = (n_indet_partido / n_partido)*100)
-
-#salvando a tabela final dos resumos dos partidos
-write.table(filia_partidos, "filiados_partidos.csv", sep = ";", dec=",", fileEncoding ="UTF-8", row.names = F)
